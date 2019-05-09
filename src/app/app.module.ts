7c15441b30ebaaefb48c85ecf54310513cfde379
Denis Yaschenko <denis1999yaschenko@gmail.com>
2019-05-09 10:44:30 +0300

    create base structure for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule
